08cb60385d6d3dbedb8ab593f08873ffcd187c7d
Anurag Garg <garganurag3955@gmail.com>
2023-08-11 17:04:45 +0530

    updating app.js

--- app.js
+++ app.js
@@ -1,93 +1,178 @@
-//jshint esversion:6
 
+//jshint esversion:6
+ 
+//requiring the modules : 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _=require("lodash");
-const mongoose = require("mongoose");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const _ = require("lodash");
+const mongoose = require('mongoose');
+main().catch((err) => console.log("err"));
+const homeStartingContent = "Ambition and determination are the key ingredients that fuel success. Stay focused on your goals, driven by your passion, and let your determination guide you to the heights of achievement.";
+const aboutContent = "This is a blog website created by Anurag Garg as a part of Web Development Course on Udemy by Angela Yu";
+const contactContent = "Contact me at garganurag3955@gmail.com for queries!";
+ 
 const app = express();
 
-app.set('view engine', 'ejs');
+//Added by me which is not present in Lecture(IMP !!!!!)
+const request = require("request");
+const cors = require('cors')
+require("dotenv").config();
 
+app.use(bodyParser.json());
+app.use(cors())
+// require these installations for env file to protect password(IMP!!!!!)
+//npm i require body-parser cors dotenv
+
+
+ 
+app.set('view engine', 'ejs');
+ 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+ 
+ 
+//Connecting to the database using mongoose.
+main().catch(err => console.log(err));
+async function main() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  await mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+}
 
- mongoose.connect("mongodb://localhost:27017/blogsDB", { useUnifiedTopology: true, useNewUrlParser: true });
-// mongoose.connect("mongodb+srv://anurag:anurag@mernapp.iip63bc.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true });
-
-// Post Schema
+// async function main() {
+//   const MONGODB_URI = process.env.MONGODB_URI;
+//   await mongoose.connect(MONGODB_URI);
+// }
+ 
+//Creating an empty array but we are not using it in this version of the app.
+// const posts = [];
+ 
+ 
+//Creating Schema for the posts 
 const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "No title specified."]
-  },
-  content: {
-    type: String,
-    required: [true, "No content specified."]
-  }
+  title : String,
+  content: String,
 });
-
-const Post = mongoose.model("Post", postSchema);
-
-// Home Route - GET
-app.get("/",function(req,res){
-  Post.find({}, function (err, foundPosts) {
-    res.render("home",{
-      startingContent:homeStartingContent,
-      posts:foundPosts
+ 
+ 
+//Creating a mongoose model based on this Schema :
+ 
+const Post = mongoose.model("Post",postSchema);
+ 
+//Adding delete button by PostId requirements
+const ObjectId = require("mongoose").Types.ObjectId;
+//
+app.get("/", function(req, res) {
+ 
+  // Find all items in the Posts collection and render it into our home page.
+ Post.find().then(posts =>{
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+        });
     });
-  });
 });
-
-// About Route - GET
-app.get("/about",function(req,res){
-  res.render("about",{aboutContent:aboutContent});
+ 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
-
-// Contact Route - GET
-app.get("/contact",function(req,res){
-  res.render("contact",{contactContent:contactContent});
+ 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
-
-// Compose Route - GET
-app.get("/compose",function(req,res){
+ 
+app.get("/compose", function(req, res){
   res.render("compose");
+ 
+ });
+ 
+ //Saved the title and the post into our blogDB database.
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  })
+ 
+  //We are saving the post through our compose route and redirecting back into the home route. A message will be displayed in our console when a post is being saved.
+ 
+  post.save().then(() => {
+ 
+    console.log('Post added to DB.');
+ 
+    res.redirect('/');
+ 
+  })
+ 
+  .catch(err => {
+ 
+    res.status(400).send("Unable to save post to database.");
+ 
+  });
+ 
+ 
+});
+ 
+app.get("/posts/:postId", function(req, res){
+  //We are storing the _id of our created post in a variable named requestedPostId
+  const requestedPostId = req.params.postId;
+
+
+ 
+  //Using the find() method and promises (.then and .catch), we have rendered the post into the designated page.
+ 
+  Post.findOne({_id:requestedPostId})
+  .then(function (post) {
+    res.render("post", {
+            title: post.title,
+            content: post.content,
+            post:post,
+          });
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+ 
+ 
+});
+ 
+
+
+//delete post
+app.get("/delete/:postId", (req, res) => {
+  const postId = req.params.postId;
+  // console.log(postId);
+  Post.findByIdAndRemove({ _id: postId })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-// Compose Route - POST
-app.post("/compose",function(req,res){
-  const post = new Post({
-    title:req.body.postTitle,
-    content:req.body.postBody
-  });
 
-  post.save(function(err){
-    if (!err){
+app.post("/delete", function(req, res) {
+  const postId = req.body.postId;
+  
+  Post.findByIdAndRemove(postId, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully deleted the post.");
       res.redirect("/");
     }
   });
 });
 
-// POSTS Route - GET
-app.get("/posts/:postID",function(req,res){
-  const reqPostID=req.params.postID;
-  Post.findOne({_id: reqPostID}, function(err, post){
 
-    res.render("post", {
- 
-      post:post
- 
-    });
- 
-  });
-});
 
-// Server Listener
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+
+//To choose deafault port provided by hosting platform
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function () {
+    console.log("Server is running on port 3000 or " + port);
 });
